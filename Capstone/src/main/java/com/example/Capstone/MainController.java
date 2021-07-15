502ccea16fd21741ca3aabb0b69f075c3414b62f
Gregory Bowles <71733838+Sbowles21@users.noreply.github.com>
2021-07-15 10:05:32 -0500

    Main Controller and html file
    Adding Dependencies, MainController, Database link, html for create

---
+++ Capstone/src/main/java/com/example/Capstone/MainController.java
@@ -0,0 +1,24 @@
+package com.example.Capstone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String homepage(){
+        return "";
+    }
+
+    @GetMapping("/pokecreate")
+    public String pokecreate(){return "pokecreate";}
+}
